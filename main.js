134f883bc3470b2b92c28b6cbe51d853d84f11da
EscTec2033 <t.contreras0716@gmail.com>
2024-09-03 20:21:12 -0400

    Problema de los 3 numeros
    Problema de los 3 numeros

---
+++ main.js
@@ -0,0 +1,15 @@
+const nu1 = prompt("ingresa el numero 1")
+const nu2 = prompt("ingresa el numero 2")
+const nu3 = prompt("ingresa el numero 3")
+
+const nu1Int = Number(nu1)
+const nu2Int = Number(nu2)
+const nu3Int = Number(nu3)
+
+const resultado = nu1Int + nu2Int + nu3Int
+
+const promedio = resultado / 3
+
+alert("Promedio de los 3 numeros es: " + promedio)
+
+
